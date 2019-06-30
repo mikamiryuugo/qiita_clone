084940dd0a8aad17f28cc6a643bd0fff86389ace
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-06-30 19:13:24 +0900

    rubocopの設定修正

--- config/application.rb
+++ config/application.rb
@@ -17,11 +17,11 @@ module QiitaClone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.generators do |g|
-      g.javascripts false
-      g.stylesheets false
-      g.helper false
-      g.test_framework :rspec
-    end
+    # config.generators do |g|
+    #   g.javascripts false
+    #   g.stylesheets false
+    #   g.helper false
+    #   g.test_framework :rspec
+    # end
   end
 end
---
+++ config/initializers/generators.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Rails.application.config.generators do |g|
+  g.template_engine false
+  g.javascripts false
+  g.stylesheets false
+  g.helper false
+  g.test_framework :rspec,
+                   fixtures: true,
+                   view_specs: false,
+                   routing_specs: false,
+                   helper_specs: false,
+                   controller_specs: false,
+                   request_specs: false
+  g.fixture_replacement :factory_bot, dir: "spec/factories"
+end
