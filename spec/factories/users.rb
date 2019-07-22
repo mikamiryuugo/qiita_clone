3b797070bfb3e3a412c7319714e77b9d1e4b3076
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-07-22 20:37:35 +0900

    factorybotのuserを消したけど、revertで打ち消したい

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :user do
-    sequence(:name) { |n| "#{n}_" + Faker::Name.name }
     sequence(:email) { |n| "#{n}_" + Faker::Internet.email }
     password { Faker::Internet.password(8) }
   end
