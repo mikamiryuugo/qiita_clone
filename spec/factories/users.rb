7fef0c92a7d3aa101a58da51fcec7d8ddb9ea6d1
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-07-22 20:42:17 +0900

    FactoryBotでnameのsequence設定を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:name) { |n| "#{n}_" + Faker::Name.name }
+    name { Faker::Name.name }
     sequence(:email) { |n| "#{n}_" + Faker::Internet.email }
     password { Faker::Internet.password(8) }
   end
