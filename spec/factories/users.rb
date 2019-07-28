38ac0a73b0ed23c0c0acbc094ef06d4d1eb18782
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-07-29 07:57:45 +0900

    userのFactoryBot修正でtrait追加。関連するarticleも同時に作成可能にした

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,18 @@ FactoryBot.define do
     name { Faker::Name.name }
     sequence(:email) { |n| "#{n}_" + Faker::Internet.email }
     password { Faker::Internet.password(8) }
+
+    trait :with_article do
+      transient do
+        article_count {5}
+      end
+
+      after(:create) do |user, evaluator|
+        evaluator.article_count.times do
+          create(:article, user: user)
+        end
+      end
+    end
+
   end
 end
