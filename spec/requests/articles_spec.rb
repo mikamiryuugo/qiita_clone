6b360596cd2a41e11ae546ebcd0f4fd9f990cd77
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-08-03 17:00:08 +0900

    articles#indexのテストコードの実装

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
-  describe "GET /articles" do
-    it "works! (now write some real specs)" do
-      get articles_path
-      expect(response).to have_http_status(200)
+  describe "GET /api/v1/articles" do
+    subject { get(api_v1_articles_path) }
+
+    before do
+      create(:user, :with_articles)
     end
+
+    it "articlesの一覧を取得できる" do
+      subject
+      res = JSON.parse(response.body).count
+      expect(res).to eq 5
+    end
+
   end
 end
