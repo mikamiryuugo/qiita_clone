eaf24a8c6f948308e0564466aceb77b4cf3d6e0f
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-08-05 07:24:32 +0900

    articles#createの実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,4 +5,16 @@ class Api::V1::ArticlesController < ApplicationController
     render json: articles
   end
 
+  def create
+    article = current_user.articles.build(article_params)
+    article.save!
+    render json: article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :user_id)
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def current_user
+    User.last
+  end
 end
