a7f53454862fee53fb30c42eb92cd85f5f4f9175
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-08-04 10:44:06 +0900

    マジックナンバー修正

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe "Articles", type: :request do
   describe "GET /api/v1/articles" do
     subject { get(api_v1_articles_path) }
 
+    let(:article_count) { 5 }
     before do
-      create(:user, :with_articles)
+      create(:user, :with_articles, article_count: article_count)
     end
 
     it "articlesの一覧を取得できる" do
       subject
       res = JSON.parse(response.body).count
-      expect(res).to eq 5
+      expect(res).to eq article_count
     end
 
   end
