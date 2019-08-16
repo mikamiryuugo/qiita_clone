27a7bba2d8f332e777a960a92cd9fae32d9a3c60
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-08-17 08:50:52 +0900

    インデントの修正

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -14,36 +14,36 @@ RSpec.describe "Articles", type: :request do
       res = JSON.parse(response.body).count
       expect(res).to eq article_count
     end
+  end
 
-    describe "POST /api/v1/articles" do
-      subject { post(api_v1_articles_path, params: params) }
+  describe "POST /api/v1/articles" do
+    subject { post(api_v1_articles_path, params: params) }
 
-      let(:current_user) { create(:user) }
-      let(:params) { { article: attributes_for(:article) } }
+    let(:current_user) { create(:user) }
+    let(:params) { { article: attributes_for(:article) } }
 
-      before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user) }
-      context "自分の記事を作成するとき" do
-        it "articleのレコードが作成できる" do
-          expect { subject }.to change { current_user.articles.count }.by(1)
-          expect(response).to have_http_status(200)
-        end
+    before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user) }
+    context "自分の記事を作成するとき" do
+      it "articleのレコードが作成できる" do
+        expect { subject }.to change { current_user.articles.count }.by(1)
+        expect(response).to have_http_status(200)
       end
     end
+  end
 
-    describe "PATCH /api/v1/articles/:id" do
-      subject { patch(api_v1_article_path(article.id), params: params) }
+  describe "PATCH /api/v1/articles/:id" do
+    subject { patch(api_v1_article_path(article.id), params: params) }
 
-      let(:article) { create(:article, user_id: current_user.id) }
-      let(:current_user) { create(:user) }
-      before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user) }
-      let(:params) { { article: attributes_for(:article) } }
+    let(:article) { create(:article, user_id: current_user.id) }
+    let(:current_user) { create(:user) }
+    before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user) }
+    let(:params) { { article: attributes_for(:article) } }
 
-      context "自分の記事のレコードを更新しようとするとき" do
-        it "articleのレコードが更新できる" do
-          expect { subject }.to change { Article.find(article.id).title }.from(article.title).to(params[:article][:title]) &
-                                change { Article.find(article.id).content }.from(article.content).to(params[:article][:content])
-          expect(response).to have_http_status(:ok)
-        end
+    context "自分の記事のレコードを更新しようとするとき" do
+      it "articleのレコードが更新できる" do
+        expect { subject }.to change { Article.find(article.id).title }.from(article.title).to(params[:article][:title]) &
+                              change { Article.find(article.id).content }.from(article.content).to(params[:article][:content])
+        expect(response).to have_http_status(:ok)
       end
     end
   end
