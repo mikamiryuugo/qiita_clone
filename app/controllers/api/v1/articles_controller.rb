14d5e53aa16563dc552cdd2e00521a52f63d53d7
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-08-18 09:03:46 +0900

    before_actionにdestroy追加

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArticlesController < ApplicationController
-  before_action :set_article, only: [:update]
+  before_action :set_article, only: [:update, :destroy]
 
   def index
     articles = Article.all
