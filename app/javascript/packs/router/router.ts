cdffd56fe5bb00a31667fb9d645a910d8af4c112
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-06 07:32:34 +0900

    「フロントエンドつなぎこみ１」で漏れがあったので修正

--- app/javascript/packs/router/router.ts
+++ app/javascript/packs/router/router.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue/dist/vue.esm.js'
 import VueRouter from 'vue-router'
-import ArticleList from '../components/ArticleList.vue'
+import ArticlesConmponent from '../components/ArticlesConmponent.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: ArticleList },
+    { path: '/', component: ArticlesConmponent },
   ],
 })
