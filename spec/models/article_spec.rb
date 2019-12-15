7958ca5c5eac7050b05f82045ab93104bc807a96
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-15 20:22:54 +0900

    下書き記事のテストコード作成

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Article, type: :model do
 
     context "statusカラムの値が1のレコードを検索した時" do
       it "公開記事が返される" do
-        expect(user.articles.where(status: 0).count).to eq 1
+        expect(user.articles.where(status: 1).count).to eq 1
       end
     end
 
     context "statusカラムの値が0の記事を指定した時" do
       it "下書き記事が返される" do
-        expect(user.articles.where(status: 1).count).to eq 1
+        expect(user.articles.where(status: 0).count).to eq 1
       end
     end
 
