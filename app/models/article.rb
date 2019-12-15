eecffb366022e3f7b3fcbf1e06ff462716d5ee19
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-15 10:33:36 +0900

    記事作成時のデフォルトステータスを「下書き」に修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
-  enum status: { published: 0, draft: 1 }
+  enum status: { draft: 0, published: 1 }
   belongs_to :user
   has_many :article_likes, dependent: :destroy
   has_many :comments, dependent: :destroy
