c7e99fb84ccded98200416afa58183fc09d7ebbd
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-18 07:56:29 +0900

    statusカラムをstring型にしたので、その修正

--- app/controllers/api/v1/drafts_controller.rb
+++ app/controllers/api/v1/drafts_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DraftsController < Api::V1::BaseApiController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    articles = current_user.articles.where(status: 0)
+    articles = current_user.articles.where(status: "draft")
     render json: articles
   end
 
