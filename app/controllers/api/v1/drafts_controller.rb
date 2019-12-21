6a7446d3b4b3fe54d84735f424975922b90f324d
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-21 13:17:56 +0900

    drafts#indexアクションをenumのメソッドを使いシンプルになるようにコード修正

--- app/controllers/api/v1/drafts_controller.rb
+++ app/controllers/api/v1/drafts_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DraftsController < Api::V1::BaseApiController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    articles = current_user.articles.where(status: "draft")
+    articles = current_user.articles.draft
     render json: articles
   end
 
