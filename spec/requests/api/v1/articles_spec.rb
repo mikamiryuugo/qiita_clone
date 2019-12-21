fb0487ea3103e26c37e0e4e6a8515415f2d75e3a
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-21 18:38:17 +0900

    articles#indexのテストコード修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe "Articles", type: :request do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
 
-    let!(:article1) { create(:article, updated_at: 1.days.ago) }
-    let!(:article2) { create(:article, updated_at: 2.days.ago) }
-    let!(:article3) { create(:article) }
-
+    let!(:article1) { FactoryBot.create(:article, :with_published_article, updated_at: 1.days.ago) }
+    let!(:article2) { FactoryBot.create(:article, :with_published_article, updated_at: 2.days.ago) }
+    let!(:article3) { FactoryBot.create(:article, :with_published_article) }
     it "記事の一覧が取得できる(更新順)" do
       subject
 
@@ -17,7 +16,7 @@ RSpec.describe "Articles", type: :request do
         expect(response).to have_http_status(:ok)
         expect(res.length).to eq 3
         expect(res.map {|d| d["id"] }).to eq [article1.id, article2.id, article3.id]
-        expect(res[0].keys).to eq ["id", "title","content", "updated_at", "user"]
+        expect(res[0].keys).to eq ["id", "title","content", "updated_at", "status", "user"]
         expect(res[0]["user"].keys).to include "id", "name", "email"
       end
     end
