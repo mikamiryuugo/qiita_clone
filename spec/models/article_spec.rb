29a6c503bd6afafa38f1340b22c1bd391bca7cb3
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-22 11:29:09 +0900

    modles/articles_apecを可読性高くなるよう修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Article, type: :model do
 
     context "statusカラムの値が「published」のレコードを検索した時" do
       it "公開記事が返される" do
-        expect(Article.published.count).to eq 1
+        expect(user.articles.published.count).to eq 1
       end
     end
 
     context "statusカラムの値が「draft」の記事を指定した時" do
       it "下書き記事が返される" do
-        expect(Article.draft.count).to eq 1
+        expect(user.articles.draft.count).to eq 1
       end
     end
 
