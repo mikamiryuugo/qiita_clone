c5b3ae8db3c0af541bd8b890e9f402c9e0b7a159
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-22 11:26:13 +0900

    タイポ修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Article, type: :model do
     let!(:article)  { create(:article, user_id: user.id) }
     let!(:article2) { create(:article, :with_published_article, user_id: user.id) }
 
-    context "statusカラムの値が「publishe」のレコードを検索した時" do
+    context "statusカラムの値が「published」のレコードを検索した時" do
       it "公開記事が返される" do
         expect(Article.published.count).to eq 1
       end
