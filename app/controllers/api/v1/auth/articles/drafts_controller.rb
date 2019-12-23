eea93aceebe2eb014ea87f183f4f3bc2b550decc
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-23 21:21:05 +0900

    draftコントローラファイルを置く階層wo修正

--- app/controllers/api/v1/auth/articles/drafts_controller.rb
+++ app/controllers/api/v1/auth/articles/drafts_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::DraftsController < Api::V1::BaseApiController
+class Api::V1::Articles::DraftsController < Api::V1::BaseApiController
   before_action :authenticate_user!, only: [:index]
 
   def index
