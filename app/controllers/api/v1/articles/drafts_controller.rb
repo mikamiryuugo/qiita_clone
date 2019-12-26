167be0acafd62a94b96d9dba44d1fabd6eb82cd0
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-27 08:20:07 +0900

    drafts_specにheaderを追記する修正

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::Articles::DraftsController < Api::V1::BaseApiController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    binding.pry
     articles = current_user.articles.draft
     render json: articles
   end
--- spec/requests/api/articles/drafts_spec.rb
+++ spec/requests/api/articles/drafts_spec.rb
@@ -8,30 +8,17 @@ RSpec.describe "Drafts", type: :request do
     let!(:article3) { create(:article, :published, user_id: user.id) }
 
     context "ログインしている時" do
-
-      let(:params) { { email: user.email, password: user.password } }
-
+      let(:headers) { authentication_headers_for(user) }
       it "下書き記事の一覧が取得できる" do
-        post(api_v1_user_session_path, params: params)
-        headers = response.headers
-
-        aggregate_failures do
-          expect(response).to have_http_status(:ok)
-          expect(headers["access-token"]).to be_present
-          expect(headers["client"]).to be_present
-          expect(headers["expiry"]).to be_present
-          expect(headers["uid"]).to be_present
-          expect(headers["token-type"]).to be_present
-        end
-
-        get(api_v1_articles_draft_path)
+        get(api_v1_articles_drafts_path, headers: headers)
         expect(user.articles.draft.count).to eq 2
       end
     end
 
     context "ログインしていない時" do
+
       it "下書き記事の一覧が取得できない" do
-        get (api_v1_articles_draft_path)
+        get (api_v1_articles_drafts_path)
         expect(response).to have_http_status(401)
       end
     end
