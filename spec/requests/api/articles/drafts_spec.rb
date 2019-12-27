9d181bbb2a288f02ef5f7b8e78ad98dc821f40ee
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-27 09:05:00 +0900

    drafts#showのテストコード作成

--- spec/requests/api/articles/drafts_spec.rb
+++ spec/requests/api/articles/drafts_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "Drafts", type: :request do
-  describe "GET /drafts/index" do
-    let!(:user) {create(:user)}
-    let!(:article)  { create(:article, user_id: user.id) }
-    let!(:article2) { create(:article, user_id: user.id) }
-    let!(:article3) { create(:article, :published, user_id: user.id) }
+  let!(:user) {create(:user)}
+  let!(:article)  { create(:article, user_id: user.id) }
+  let!(:article2) { create(:article, user_id: user.id) }
+  let!(:article3) { create(:article, :published, user_id: user.id) }
+  let(:headers) { authentication_headers_for(user) }
 
+  describe "GET /drafts/index" do
     context "ログインしている時" do
-      let(:headers) { authentication_headers_for(user) }
       it "下書き記事の一覧が取得できる" do
         get(api_v1_articles_drafts_path, headers: headers)
         expect(user.articles.draft.count).to eq 2
@@ -16,10 +16,46 @@ RSpec.describe "Drafts", type: :request do
     end
 
     context "ログインしていない時" do
-
       it "下書き記事の一覧が取得できない" do
         get (api_v1_articles_drafts_path)
         expect(response).to have_http_status(401)
+      end
+    end
+  end
+
+  describe "GET /drafts/:id" do
+    let(:article_id) {article.id}
+
+    context "ログインしている時" do
+      it "下書き詳細記事が取得できる" do
+        get(api_v1_articles_draft_path(article_id), headers: headers)
+
+        res = JSON.parse(response.body)
+
+        aggregate_failures do
+          expect(response).to have_http_status(:ok)
+          expect(res["id"]).to eq article.id
+          expect(res["title"]).to eq article.title
+          expect(res["content"]).to eq article.content
+          expect(res["updated_at"]).to be_present
+          expect(res["user"]["id"]).to eq article.user.id
+          expect(res["user"].keys).to include "id", "name", "email"
+        end
+      end
+    end
+
+    context "ログインしていない時" do
+      it "下書き詳細記事が取得できない" do
+        get(api_v1_articles_draft_path(article_id))
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context "指定した id の下書き記事が存在しない場合" do
+      let(:article_id) { 10000 }
+
+      it "記事が見つからない" do
+        expect { get(api_v1_articles_draft_path(article_id), headers: headers) }.to raise_error ActiveRecord::RecordNotFound
       end
     end
 
