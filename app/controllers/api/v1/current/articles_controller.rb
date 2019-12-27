47a8387e9312693bba7aa3182ec27a9773ad6091
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-27 13:28:59 +0900

    ログイン前提のapiのため,only制限をなくす

--- app/controllers/api/v1/current/articles_controller.rb
+++ app/controllers/api/v1/current/articles_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Current::ArticlesController < Api::V1::BaseApiController
-  before_action :authenticate_user!, only: %i[index]
+  before_action :authenticate_user!
 
   def index
     articles = current_user.articles.published
