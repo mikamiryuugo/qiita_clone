c874a478086e543ff1046b680f081c48312eea25
mikamiryu <mikamiryuugo@yahoo.co.jp>
2019-12-28 18:33:30 +0900

    下書き用のvueファイルの追加

--- app/javascript/packs/router/router.ts
+++ app/javascript/packs/router/router.ts
@@ -5,6 +5,8 @@ import ArticleContainer from "../container/ArticleContainer.vue";
 import RegisterContainer from "../container/RegisterContainer.vue";
 import LoginContainer from "../container/LoginContainer.vue";
 import EditArticleContainer from "../container/EditArticleContainer.vue";
+import DraftArticlesContainer from "../container/DraftArticlesContainer.vue";
+import EditDraftArticleContainer from "../container/EditDraftArticleContainer.vue";
 import MyPageContainer from "../container/MyPageContainer.vue";
 
 Vue.use(VueRouter);
@@ -15,8 +17,11 @@ export default new VueRouter({
     { path: "/", component: ArticlesContainer },
     { path: "/sign_up", component: RegisterContainer },
     { path: "/sign_in", component: LoginContainer },
+    { path: "/articles/drafts", component: DraftArticlesContainer },
     { path: "/articles/new", component: EditArticleContainer },
+    { path: "/articles/drafts/:id/edit", component: EditDraftArticleContainer },
     { path: "/articles/:id/edit", component: EditArticleContainer },
+
     { path: "/articles/:id", component: ArticleContainer, name: "article" },
     { path: "/mypage", component: MyPageContainer }
   ]
